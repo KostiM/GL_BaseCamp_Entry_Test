a819a9dc012d62336789a185dd780b66c2b429ab
KostiM <mishakostitskiy@gmail.com>
2020-05-21 22:27:37 +0300

    comments in 3rd and 4th task added

--- Task_3_ls/ls.cpp
+++ Task_3_ls/ls.cpp
@@ -1,53 +1,46 @@
 #include <iostream>
-#include <dirent.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <ctime>
-#include <pwd.h>
+#include <dirent.h> // dirent struct , opendir(), closedir()
+#include <sys/types.h> // for dirent.h
+#include <sys/stat.h> // stat struct
+#include <ctime> // localtime() , tm struct
+#include <pwd.h> // getpwuid(), passwd_t
 
 using namespace std;
 
 const char chars[] = "rwxrwxrwx";
 
-char type (const struct stat mode);
+char type (const struct stat mode); // return file type flag
 
 int main(int argc, char* argv[])
 {
     DIR* dir;
     dirent* pdir;
+    dir = opendir((argc == 2 ? argv[1] : ".")); // open current or defined directory 
+    if (dir == NULL)
+    {
+        exit(1);
+    }
     struct stat st;
-    tm time;
-    const char* dirname = (argc == 2 ? argv[1] : ".");
-    dir = opendir(dirname);
-    if (dir == NULL)
-    {
-        exit(1);
-    }
-    if (dir == NULL)
-    {
-        exit(1);
-    }
     while ((pdir = readdir(dir)) != NULL)
     {
-        stat(pdir->d_name, &st);
-        time = *localtime(&st.st_mtime);
+        stat(pdir->d_name, &st); // get info about file
         char flags[10];
         for (size_t i = 0; i < 9; i++) {
-            flags[i] = (st.st_mode & (1 << (8 - i))) ? chars[i] : '-';
+            flags[i] = (st.st_mode & (1 << (8 - i))) ? chars[i] : '-'; // generate file permission flag
         }
         flags[9] = '\0';
-        cout << type(st) << flags << '\t' << st.st_nlink;
-        struct passwd uname = *getpwuid(st.st_uid);
-        cout << uname.pw_name << '\t';
-        uname = *getpwuid(st.st_gid);
-        cout << uname.pw_name << '\t';
-        cout<< st.st_size << "\t"
+        cout << type(st) << flags << '\t' << st.st_nlink; // print file type and permission info and hard link number
+        cout << (*getpwuid(st.st_uid)).pw_name << '\t'; // print owner name by id
+        cout << (*getpwuid(st.st_gid)).pw_name << '\t'; // print owner group name by id
+        tm time = *localtime(&st.st_mtime); // interpret last modification time in normal format
+        cout << st.st_size << "\t" 
+            //next 5 lines print last modification time
             << (time.tm_mday < 10 ? "0" : "") << time.tm_mday << "."
             << (time.tm_mon + 1 < 10 ? "0" : "") << time.tm_mon + 1 << "."
             << time.tm_year + 1900 << ". "
             << (time.tm_hour < 10 ? "0" : "") << time.tm_hour << ":"
             << (time.tm_min < 10 ? "0" : "") << time.tm_min << "\t"
-            << pdir->d_name << endl;
+            << pdir->d_name << endl; // print file name
     }
     closedir(dir);
     return 0;
--- Task_4_File_Extractor/FSIE.cpp
+++ Task_4_File_Extractor/FSIE.cpp
@@ -29,40 +29,41 @@ int main(int argc, char* argv[])
             dirname = argv[2];
             if (dirname.find('/', dirname.size()-1) == string::npos)
             {
-                dirname += "/";
+                dirname += "/"; // add "/" in the end of directory name if needed
             }
-            mkdir(dirname.c_str(), 0777);
+            mkdir(dirname.c_str(), 0777); // create defined directory if it doesn`t exist
         }
         //read FENTRY TABLEs for each file
         for (unsigned i = 0; i < entnum; ++i)
         {
             char buff[4];
-            file.read(buff, 3); // read 3 bytes that describe fsize
+            file.read(buff, 3); // read 3 bytes that describe fentry_table::fsize
             unsigned* buffp = reinterpret_cast<unsigned*>(buff);
             fsize[i] = *buffp;
             fsize[i] = (*buffp & 0x00FFFFFF); // erase 4th byte
-            file.read(buff, 4); // read 4 bytes that describe foffset
+            file.read(buff, 4); // read 4 bytes that describe fentry_table::foffset
             foffset[i] = *buffp;
         }
+        // read each FILE NODES and create the files
         for (unsigned i = 0; i < entnum; ++i)
         {
-            file.seekg(foffset[i]+fsize[i], ios::beg);
+            file.seekg(foffset[i]+fsize[i], ios::beg); //set get pointer to start of file path
             string filename;
             getline(file, filename, '\0'); // read file path
             if (dirname.size())
             {
-                filename = dirname + filename; // change file name if destination directory entered   
+                filename = dirname + filename; // change file name if destination directory entered
             }
             if (filename.find('/') != string::npos)
             {
-                mkdir(filename.substr(0, filename.find_last_of("/")).c_str(),0777);
+                mkdir(filename.substr(0, filename.find_last_of("/")).c_str(),0777); // create subdirectory is needed
             }
             ofstream newfile(filename, ios::out | ios::binary);
             if (newfile.is_open())
             {
-                file.seekg(foffset[i],ios::beg);
+                file.seekg(foffset[i],ios::beg); // set get pointer to start of file data
                 string buff(fsize[i],'\0');
-                file.read((char*)buff.c_str(), fsize[i]);
+                file.read((char*)buff.c_str(), fsize[i]); 
                 newfile.write((char*)buff.c_str(), fsize[i]);
                 newfile.close();
             }
